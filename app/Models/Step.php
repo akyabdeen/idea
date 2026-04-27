1febe844f9d45df33932ccfb2e8509f694fc0945
Abdeen <a.abdeen@sagerdrone.com>
2026-04-27 13:30:36 +0300

    steps + ideas set up

---
+++ app/Models/Step.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Step extends Model
+{
+    /** @use HasFactory<\Database\Factories\StepFactory> */
+    use HasFactory;
+
+    function idea() : BelongsTo {
+        return $this->belongsTo(Idea::class);
+    }
+}
